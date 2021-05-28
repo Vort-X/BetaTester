e601b637bcde36419956a84e0314776e5e757da1
Mihailo <mihailpro02@gmail.com>
2021-05-28 20:53:50 +0300

    fixed keys

--- src/Entities/QuestionEntity.cs
+++ src/Entities/QuestionEntity.cs
@@ -1,11 +1,14 @@
 ﻿using Entities.Interfaces;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataLayer.Entities
 {
     public class QuestionEntity : BaseEntity, IAggregateRoot
     {
         public string Text { get; private set; }
+        [ForeignKey("DifficultyId")]
+        public QuestionDifficultyEntity Difficulty { get; private set; }
         public int DifficultyId { get; private set; }
         public List<AnswerEntity> Answers { get; private set; }
 
--- src/Entities/TestItemEntity.cs
+++ src/Entities/TestItemEntity.cs
@@ -1,8 +1,12 @@
-﻿namespace DataLayer.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataLayer.Entities
 {
     public class TestItemEntity : BaseEntity
     {
         public bool GotRightAnswer { get; private set; }
+        [ForeignKey("QuestionId")]
+        public QuestionEntity Question { get; private set; }
         public int QuestionId { get; private set; }
 
         private TestItemEntity()
